c4f23df679c08c060db871254ad217d6cf79e802
Vladimir RM <shvdrummer@gmail.com>
2023-02-07 15:47:23 +0200

    add useSelector

---
+++ src/components/FirstName.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+const FirstName = () => {
+
+
+    const name = useSelector((state)=>state.user.firstName)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FirstName
