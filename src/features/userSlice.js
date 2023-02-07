0ddfdf80e36b79c394c9691ccbaa416bdd016bb7
Vladimir RM <shvdrummer@gmail.com>
2023-02-07 18:11:46 +0200

    add lastName in reducer

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -10,8 +10,11 @@ export const userSlice = createSlice({
     reducers:{
         setFirstName:(state,action)=>{
            state.firstName =action.payload
+        },
+        setLastName:(state,action)=>{
+            state.lastName=action.payload
         }
     }
 })
-export const {setFirstName} = userSlice.actions
+export const {setFirstName,setLastName} = userSlice.actions
 export default userSlice.reducer
