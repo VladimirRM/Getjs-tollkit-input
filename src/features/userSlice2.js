a722efb13dd3669d5b86459449a2c8e0de4e4f97
Vladimir RM <shvdrummer@gmail.com>
2023-02-08 10:31:52 +0200

    add onChange in input

--- src/features/userSlice2.js
+++ src/features/userSlice2.js
@@ -4,17 +4,17 @@ const initialState = {
     firstName:'',
     lastName:'',
 }
-export const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers:{
-        setFirstName:(state,action)=>{
-           state.firstName =action.payload
-        },
-        setLastName:(state,action)=>{
-            state.lastName=action.payload
-        }
-    }
+// export const userSlice = createSlice({
+//     name: 'user',
+//     initialState,
+//     reducers:{
+//         setFirstName:(state,action)=>{
+//            state.firstName =action.payload
+//         },
+//         setLastName:(state,action)=>{
+//             state.lastName=action.payload
+//         }
+//     }
 })
-export const {setFirstName,setLastName} = userSlice.actions
-export default userSlice.reducer
+// export const {setFirstName,setLastName} = userSlice.actions
+// export default userSlice.reducer
