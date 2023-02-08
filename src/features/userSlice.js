4f54456844e8bc6476dc8376ff0d1184f9bb3224
Vladimir RM <shvdrummer@gmail.com>
2023-02-08 10:02:59 +0200

    create action setLastName

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -11,6 +11,9 @@ const userSlice = createSlice({
     reducers:{
         setFIrstName:(state,action)=>{
             state.firstName=action.payload
+        },
+        setLastName:(state,action)=>{
+            state.lastName=action.payload
         }
     }
 })
